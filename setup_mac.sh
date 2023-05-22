9c25a9f12dee03692ba7db422cede80f14008021
kenta yamamoto <yamamoto7.pgming@gmail.com>
2023-05-22 22:25:24 +0900

    add: commands

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,4 +1,13 @@
 
-# brew install anyenv
 brew install neovim
+# in neovim :PlugInstall
+brew install anyenv
+anyenv install --init
+# anyenv install pyenv
+# anyenv install nodenv
+# anyenv install rbenv
+# anyenv install goenv
+# anyenv install jenv
+# anyenv install phpenv
+
 
