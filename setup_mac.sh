d2f1e61ba3e5a113d34006cccb1d69d4e0ab5ae9
kenta.yamamoto <yamamoto7.pgming@gmail.com>
2024-03-10 21:04:25 +0900

    add: settings

--- setup_mac.sh
+++ setup_mac.sh
@@ -7,7 +7,7 @@ brew install neovim
 brew install anyenv
 anyenv install --init
 # anyenv install pyenv
-# anyenv install nodenv
+anyenv install nodenv
 # anyenv install rbenv
 # anyenv install goenv
 # anyenv install jenv
@@ -15,3 +15,4 @@ anyenv install --init
 
 brew install peco
 brew install bash-completion
+brew install jq
